1bc79ae42fb0078a5f2b51fa58d5bfa2e2489fb1
maeveBestDev <77807261+maeveBestDev@users.noreply.github.com>
2021-03-05 11:20:54 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function controle(){
+var saisie =document.getElementById("input").value;
+if (saisie == "anime"){
+  document.location.href="https://nsi-team.github.io/Anime/";
+  }
+if (saisie == "jeux vidéos"){
+    document.location.href="https://nsi-team.github.io/Jeux_Videos/";
+  }
+if(saisie == "pixel art"){
+  document.location.href="https://nsi-team.github.io/Pixel_Art/";
+}
+if(saisie == "marrant"){
+  document.location.href="https://nsi-team.github.io/Marrant/";
+}
+if(saisie == "chat"){
+  document.location.href="https://nsi-team.github.io/Chat/";
+}
+
+}
